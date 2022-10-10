fd66dc647523d2682114c7dbab29c0d70f9ad38e
jgoux <hi@jgoux.dev>
2022-10-10 18:05:32 +0200

    change setup url

--- scripts/setup.mjs
+++ scripts/setup.mjs
@@ -30,7 +30,7 @@ if (process.env.VERCEL_TEAM_ID && !project?.env?.find(env => env.key === "VERCEL
   console.log("VERCEL_TEAM_ID environment variable created.");
 }
 
-const commandForIgnoringBuildStep = `curl -sS "https://raw.githubusercontent.com/snaplet/vercel-action/main/scripts/ignore-build.mjs" | node --input-type=module`;
+const commandForIgnoringBuildStep = `curl -sS "https://raw.githubusercontent.com/snaplet/vercel-action/fix-add-vercel-team-id/scripts/ignore-build.mjs" | node --input-type=module`;
 if (project?.commandForIgnoringBuildStep !== commandForIgnoringBuildStep) {
   console.log("Setting Ignored Build Step Command...");
   await vercel("/", {
