6517418ca27406d923c26e465a8f47802c8336cc
Anders Bech Mellson <mellson@icloud.com>
2022-10-10 18:18:05 +0000

    print deploy hook

--- scripts/deploy.mjs
+++ scripts/deploy.mjs
@@ -1,42 +1,46 @@
-import { vercel } from "./vercel.mjs";
+import { vercel } from './vercel.mjs';
 
-const project = await vercel("/");
+const project = await vercel('/');
 
-let deployHookUrl = project?.link?.deployHooks?.find(deployHook => deployHook.ref === process.env.GITHUB_HEAD_REF)?.url;
+let deployHookUrl = project?.link?.deployHooks?.find(
+  (deployHook) => deployHook.ref === process.env.GITHUB_HEAD_REF
+)?.url;
 
-const deployHookExists = Boolean(deployHookUrl)
+const deployHookExists = Boolean(deployHookUrl);
 
 if (!deployHookExists) {
-  console.log("Creating deploy hook...");
-  const updatedProject = await vercel("/deploy-hooks", {
-    method: "POST",
+  console.log('Creating deploy hook...');
+  const updatedProject = await vercel('/deploy-hooks', {
+    method: 'POST',
     body: JSON.stringify({
       name: process.env.GITHUB_HEAD_REF,
       ref: process.env.GITHUB_HEAD_REF,
     }),
   });
-  deployHookUrl = updatedProject?.link?.deployHooks?.find(deployHook => deployHook.ref === process.env.GITHUB_HEAD_REF)?.url;
-  console.log("Deploy hook created.");
+  deployHookUrl = updatedProject?.link?.deployHooks?.find(
+    (deployHook) => deployHook.ref === process.env.GITHUB_HEAD_REF
+  )?.url;
+  console.log('Deploy hook created.');
+  console.log(deployHookUrl);
 
-  const environmentVariables = process.env.VERCEL_PREVIEW_ENV
-    .trim()
-    .split("\n")
-    .filter(line => line.trim().length > 0 && !line.trim().startsWith("#"))
-    .map(line => {
-      const [key, ...parts] = line.trim().split("=");
-      const value = parts.join("=");
+  const environmentVariables = process.env.VERCEL_PREVIEW_ENV.trim()
+    .split('\n')
+    .filter((line) => line.trim().length > 0 && !line.trim().startsWith('#'))
+    .map((line) => {
+      const [key, ...parts] = line.trim().split('=');
+      const value = parts.join('=');
       return { key, value };
     });
-  await Promise.all(environmentVariables
-    .map(async ({ key, value }) => {
+  await Promise.all(
+    environmentVariables.map(async ({ key, value }) => {
       console.log(`Creating ${key} environment variable...`);
-      await vercel("/env", {
-        method: "POST",
+      await vercel('/env', {
+        method: 'POST',
         body: JSON.stringify({
           gitBranch: process.env.GITHUB_HEAD_REF,
           key,
-          target: ["preview"],
-          type: "encrypted",
+          target: ['preview'],
+          type: 'encrypted',
           value,
         }),
       });
@@ -45,6 +49,6 @@ if (!deployHookExists) {
   );
 }
 
-console.log("Creating deployment...");
-await fetch(deployHookUrl, { method: "POST" });
-console.log("Deployment created.");
+console.log('Creating deployment...');
+await fetch(deployHookUrl, { method: 'POST' });
+console.log('Deployment created.');
